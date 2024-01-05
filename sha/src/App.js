368872442710fa5a01c94524a67d2746dfe42ee4
alishash05 <alisha05.shaikh@gmail.com>
2024-01-05 07:01:06 -1200

    test push

--- sha/src/App.js
+++ sha/src/App.js
@@ -2,14 +2,8 @@
 import './App.css';
 
 function App() {
-for (var a = 1; a <= 10; a++) 
-{
-document.write(" <tr> ");
-for (var b = 1; b <= 20; b++) 
-	{
-	var c=a*b;
-	document.write(" <td  style= padding:15px ; >  "  + c + " <br>   </td> ")
-	}
-}
+return (<>
+test
+</>);
 }
 export default App;
